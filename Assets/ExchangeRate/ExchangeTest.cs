7e305eef76e4db4b4dbcf59efe8fa439aaa93aae
Kodai - HomeWorkstation <kodai100@gmail.com>
2019-05-26 01:51:29 +0900

    為替レート計算機能追加

---
+++ Assets/ExchangeRate/ExchangeTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ExchangeTest : MonoBehaviour
+{
+
+    [Header("Base")]
+    public Currency baseCurrency = Currency.JPY;
+    public float value = 100;
+
+    [Header("Target")]
+    public Currency targetCurrency = Currency.USD;
+
+
+    async void Start()
+    {
+
+        float result = await ExchangeRateAPI.Exchange(value, baseCurrency, targetCurrency);
+
+        Debug.Log($"{result} [{targetCurrency.ToString()}]");
+    }
+
+    void Update()
+    {
+
+    }
+}
